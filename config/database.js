20fb43d15879304b18e6ffd8a53b7c96c819ea6f
NageshSannakkollu <122163329+NageshSannakkollu@users.noreply.github.com>
2025-03-17 07:43:42 +0530

    uri update

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const uri = process.env.MONGO_URI
 // console.log("Uri:",uri)
 const DbConnection = async() => {
     try {
-        await mongoose.connect(`${uri}`)
+        await mongoose.connect(uri)
         console.log("DB Connected Successfully!")
     } catch (error) {
         console.log(`DB Error: ${error.message}`)
