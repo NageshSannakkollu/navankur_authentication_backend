1e695f7f536ba5d5043c52889dd4a9f4fbc38a08
NageshSannakkollu <122163329+NageshSannakkollu@users.noreply.github.com>
2025-03-17 07:37:15 +0530

    mongouri updated

--- config/database.js
+++ config/database.js
@@ -2,6 +2,7 @@ const { default: mongoose, model } = require("mongoose")
 require("dotenv").config()
 
 const uri = process.env.MONGO_URI
+// console.log("Uri:",uri)
 const DbConnection = async() => {
     try {
         await mongoose.connect(`${uri}`)
